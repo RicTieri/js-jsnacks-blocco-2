b85258c38b6e8b60b49ca9bb1fe709e08f242590
RicTieri <tieri.riccardo@gmail.com>
2023-11-17 11:30:02 +0100

    jsnack blocco2 ex1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,10 +1,14 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
-const array = []
+const array = [];
+let sum = 0;
+let i = 0;
 
-while ( array.length < 50){
-  const newNumber = parseInt(prompt('insert a number'))
-  array.push(newNumber)
+while ( sum < 50){
+  const newNumber = parseInt(prompt('insert a number'));
+  array.push(newNumber);
+  sum += array[i];
+  i++;
 }
 
 console.log(array)
