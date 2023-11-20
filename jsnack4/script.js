4fc11272131fdb82c605613efada67ce6da1f589
RicTieri <tieri.riccardo@gmail.com>
2023-11-20 12:14:47 +0100

    snack4

---
+++ jsnack4/script.js
@@ -0,0 +1,24 @@
+// Crea una funzione che accetta due numeri, se il primo e' divisibile per il secondo, allora ritorna vero, altrimenti ritorna falso. Micro bonus: se sono uguali ritorna il numero.
+
+
+const input1 = parseInt(prompt('type a number'))
+const input2 = parseInt(prompt('type a number'))
+
+console.log(numberDividend(input1, input2))
+
+
+
+
+
+
+
+
+function numberDivisibleBy(numOne, numTwo){
+  if (numOne === numTwo){
+    return numOne
+  }
+  if (numOne % numTwo === 0){
+    return true
+  }
+  return false
+}
