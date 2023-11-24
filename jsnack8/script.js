3ae186404017462917393c7e5c8ba053c2f60d5d
RicTieri <tieri.riccardo@gmail.com>
2023-11-24 12:37:26 +0100

    added first print

--- jsnack8/script.js
+++ jsnack8/script.js
@@ -1,54 +1,55 @@
 // Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+const wrapperEl = document.querySelector('.wrapper');
 
 const zucchiniType = [
   {
     variety: 'nero di Milano',
     weight: 300,
-    lenght: 25,
+    lenght: 28,
   },
   {
     variety: 'romanesco',
-    weight: 300,
-    lenght: 25,
+    weight: 220,
+    lenght: 27,
   },
   {
     variety: 'ortolano di Faenza',
-    weight: 300,
-    lenght: 25,
+    weight: 285,
+    lenght: 24,
   },
   {
     variety: 'lunga fiorentina',
-    weight: 300,
-    lenght: 25,
+    weight: 271,
+    lenght: 29,
   },
   {
     variety: 'siciliano',
-    weight: 300,
-    lenght: 25,
+    weight: 253,
+    lenght: 21,
   },
   {
     variety: 'striata di Napoli',
-    weight: 300,
-    lenght: 25,
+    weight: 221,
+    lenght: 26,
   },
   {
     variety: 'bianca triestina',
-    weight: 300,
-    lenght: 25,
+    weight: 276,
+    lenght: 35,
   },
   {
     variety: 'rigata pugliese',
-    weight: 300,
-    lenght: 25,
+    weight: 210,
+    lenght: 18,
   },
   {
     variety: 'tondo di Piacenza',
-    weight: 300,
-    lenght: 25,
+    weight: 200,
+    lenght: 33,
   },
   {
     variety: 'tondo di Nizza',
-    weight: 300,
+    weight: 380,
     lenght: 25,
   },
 ]
@@ -57,7 +58,7 @@ let totalWeight = 0
 
 for (let index = 0; index < zucchiniType.length; index++) {
   const zucchina = zucchiniType[index];
-  
   totalWeight += zucchina.weight;
 }
-console.log(totalWeight)
+
+wrapperEl.innerHTML = totalWeight + 'g'
